20512ffc63dbba1845a23bc85f6440660c0c69c9
DESKTOP-DOSANLO\ayup1 <guillaume.villee@irisib.be>
2019-03-16 02:00:51 +0100

    PauseMenu + Button sprites

---
+++ BlendingJam2019Game/Assets/Guillaume/PauseMenuBehaviour.cs
@@ -0,0 +1,56 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class PauseMenuBehaviour : MonoBehaviour
+{
+    // canvas
+    private GameObject menu;
+
+    // is the game pause or not
+    private bool isPaused = false;
+
+    // Use this for initialization
+    void Start()
+    {
+        menu = GameObject.FindGameObjectWithTag("Menu");
+        menu.SetActive(false);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Escape))
+        {
+            isPaused = !isPaused;
+            if (isPaused)
+            {
+                menu.SetActive(true);
+                Time.timeScale = 0.0f;
+            }
+            else
+            {
+                menu.SetActive(false);
+                Time.timeScale = 1.0f;
+            }
+        }
+    }
+
+    public void OnResumeButtonClicked()
+    {
+        menu.SetActive(false);
+        Time.timeScale = 1.0f;
+    }
+
+    public void OnRetryButtonClicked()
+    {
+        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex);
+        Time.timeScale = 1.0f;
+    }
+
+    public void OnQuitButtonCLicked()
+    {
+
+    }
+}
